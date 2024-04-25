10da59adce3b858f90f0eded79a8d2289ddd40ae
Miguel Navarro <miguel@flowpop.co>
2024-04-25 12:32:42 -0600

    modified hello-world function

--- supabase/functions/hello-world/index.ts
+++ supabase/functions/hello-world/index.ts
@@ -1,32 +1,18 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
+import express from 'npm:express@4.18.2'
 
-// Setup type definitions for built-in Supabase Runtime APIs
-/// <reference types="https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts" />
+const app = express()
+app.use(express.json())
+const port = 3000
 
-console.log("Hello from Functions!")
-
-Deno.serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
-  }
-
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
+app.get('/hello-world', (req, res) => {
+  res.send('Hello World!')
 })
 
-/* To invoke locally:
+app.post('/hello-world', (req, res) => {
+  const { name } = req.body
+  res.send(`Hello ${name}!`)
+})
 
-  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
-  2. Make an HTTP request:
-
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/hello-world' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
-
-*/
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
